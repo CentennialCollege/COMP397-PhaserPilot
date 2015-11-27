466d3844e6f2d189813e2adf274ee07ffd22c43a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-11-27 17:09:25 -0500

    updated project to include all files

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="bootstrap/bootstrap.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="phaser/phaser.d.ts" />
+/// <reference path="phaser/p2.d.ts" />
+/// <reference path="pixi.js/pixi.d.ts" />
